ea5a8824ddd6e49220be1dba134313ece28a3bb7
thisisteey <dadataiwo82@yahoo.com>
2024-07-16 01:03:19 -0700

    Feature: Implement in-place 90-degree clockwise rotation for an n x n 2D matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-
-""" Rotate 2D Matrix 90 Degrees Clockwise"""
+"""Module for performing in-place rotation of a 2D matrix"""
 
 
 def rotate_2d_matrix(matrix):
-    """ Function for rotating 2D Matrix 90 degrees clockwise
-    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
-    - Do not return anything. The matrix must be edited in-place.
-    - You can assume the matrix will have 2 dimensions and will not be empty.
-    """
-
-    n = len(matrix[0])
-
-    for i in range(n - 1, -1, -1):
-        for j in range(0, n):
-            matrix[j].append(matrix[i].pop(0))
+    """In-place rotation of a rectangular 2D list (matrix)"""
+    size = len(matrix[0])
+    for col in range(size - 1, -1, -1):
+        for row in range(size):
+            matrix[row].append(matrix[col].pop(0))
